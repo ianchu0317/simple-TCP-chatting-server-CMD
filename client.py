200138bf843b1eb789e2080bef96341715deaea3
Ian Chen <71509578+ianchu0317@users.noreply.github.com>
2022-02-22 10:49:45 -0300

    need to use curses library, "curses.h"

--- client.py
+++ client.py
@@ -1,27 +1,38 @@
 from ctypes.wintypes import MSG
+import threading
 import socket
 import sys
+import curses
+
 
 class Client:
     def __init__(self, address, port):
         self.SERVER_ADDRESS = (address, port)
         self.MSG_BUFF = 1024
 
+    # Connect to server
     def connectServer(self):
         c_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         c_sock.connect(self.SERVER_ADDRESS)
 
-        CONNECTED = True
-        while CONNECTED:
-            message = str(c_sock.recv(self.MSG_BUFF), "utf-8")
-            print(message)
-            toSend = input("me: ")
-            c_sock.send(bytes(toSend, "utf-8"))
-            if toSend == "quit":
-                CONNECTED = False
-        c_sock.close()
+        # Possible entry: server or user input, using threading for receive connections
+        serverUpdate = threading.Thread(target=self.updateFromServer, args=(c_sock))
+        serverUpdate.start()
         sys.exit()
 
+    # Receive messages
+    def updateFromServer(self, c_sock):
+        CONNECTED = True
+        while CONNECTED:
+            try:
+                message = str(c_sock.recv(self.MSG_BUFF), "utf-8")
+                print(message)
+            except:
+                continue
+
+
+    def exitServer(self):
+        pass
 # Get user input for server information
 def getServerInfo():
     if len(sys.argv) == 3:
